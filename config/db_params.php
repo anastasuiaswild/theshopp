6d6c87d59e849a71335bcc99452642a00582182e
anastasuiaswild <32174146+anastasuiaswild@users.noreply.github.com>
2018-04-24 18:31:33 +0300

    Update db_params.php

--- config/db_params.php
+++ config/db_params.php
@@ -3,7 +3,8 @@
 return array(
   //'host'=>'localhost',
   'host'=>'us-cdbr-iron-east-05.cleardb.net',
-    'dbname'=>'grekova_shop',
+    //'dbname'=>'grekova_shop',
+  'dbname'=>'heroku_8c3db3438d02932',
     //'user'=>'root',
   'user'=>'bcf18aad891002',
     //'password'=>'',
